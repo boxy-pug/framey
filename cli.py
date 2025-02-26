ede2c5cb11078c801b46126d9bfa954cc282abde
boxy-pug <wewepipi99@pm.me>
2025-02-26 16:26:22 +0100

    made a working frame func

--- cli.py
+++ cli.py
@@ -10,6 +10,9 @@ def parse_arguments():
     parser.add_argument("width", help="Width of image, in cm", type=int, default=10)
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="Enable verbose output"
+    
     )
+    parser.add_argument("-o", "--output-size", type=int, help="The width of output image in px. Defaults to same as input")
+    parser.add_argument("-bc", "--background-color", default="white", help="Add background color, hex, rgb or html color names. Defaults to white")
 
     return parser.parse_args()
