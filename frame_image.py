eaa25dcd595f25201b3d4962536bdf2f8ada24af
boxy-pug <wewepipi99@pm.me>
2025-02-27 00:56:24 +0100

    added new create framed img method

--- frame_image.py
+++ frame_image.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from utils import get_new_filename
 
 
 class FramedImage:
@@ -14,7 +15,6 @@ class FramedImage:
         self.pasp_w_px, self.pasp_h_px = int(self.pasp_w_cm * self.scaling_factor), int(
             self.pasp_h_cm * self.scaling_factor
         )
-        # self.pasp_border_width_px = self.calculate_pasp_border_width_px()
         self.frame_w_cm, self.frame_h_cm = self.calculate_frame_size_cm(args)
         self.frame_w_px, self.frame_h_px = int(
             self.frame_w_cm * self.scaling_factor
@@ -23,6 +23,7 @@ class FramedImage:
         self.total_w_px, self.total_h_px = int(
             self.total_w_cm * self.scaling_factor
         ), int(self.total_h_cm * self.scaling_factor)
+        self.pasp_border_width_px = self.frame_w_px // 100
 
     def calculate_pasp_size_cm(self, args):
         p_width = 4
@@ -45,6 +46,11 @@ class FramedImage:
             args.background_width * 2
         )
 
+    def calculate_position(self, x, y):
+        w = (self.total_w_px - x) // 2
+        h = (self.total_h_px - y) // 2
+        return w, h
+
     def create_framed_picture(self):
         background = Image.new(
             "RGB", (self.total_w_px, self.total_h_px), self.args.background_color
@@ -53,40 +59,58 @@ class FramedImage:
         frame = Image.new(
             "RGB", (self.frame_w_px, self.frame_h_px), self.args.frame_color
         )
-        background.paste(
-            frame,
-            (
-                (self.total_w_px - self.frame_w_px) // 2,
-                (self.total_h_px - self.frame_h_px) // 2,
-            ),
-        )
 
         passepartout = Image.new("RGB", (self.pasp_w_px, self.pasp_h_px), "white")
-        passepartout.paste(
-            self.image,
+
+        passepartout_inner_border = Image.new(
+            "RGB",
             (
-                (self.pasp_w_px - self.art_w_px) // 2,
-                (self.pasp_h_px - self.art_h_px) // 2,
+                self.art_w_px + self.pasp_border_width_px,
+                self.art_h_px + self.pasp_border_width_px,
+            ),
+            "whitesmoke",
+        )
+
+        artwork = self.image
+
+        background.paste(
+            frame,
+            (self.calculate_position(self.frame_w_px, self.frame_h_px)),
+        )
+
+        background.paste(
+            passepartout,
+            (self.calculate_position(self.pasp_w_px, self.pasp_h_px)),
+        )
+        background.paste(
+            passepartout_inner_border,
+            (
+                self.calculate_position(
+                    self.art_w_px + self.pasp_border_width_px,
+                    self.art_h_px + self.pasp_border_width_px,
+                )
             ),
         )
         background.paste(
-            passepartout,
-            (
-                (self.total_w_px - self.pasp_w_px) // 2,
-                (self.total_h_px - self.pasp_h_px) // 2,
-            ),
+            artwork,
+            (self.calculate_position(self.art_w_px, self.art_h_px)),
         )
 
-        output_width_px = self.image.width
-        if self.args.output_size:
-            output_width_px = self.args.output_size
+        final_width, final_height = self.get_final_image_size(background)
+        final_image = background.resize((final_width, final_height))
+        new_filename = get_new_filename(self.image.filename)
+        print(new_filename)
 
-        resized = background.resize(
-            (output_width_px, int(output_width_px * self.aspect_ratio))
-        )
-        resized.save("samples/test2.jpg")
+        final_image.save(new_filename)
 
         return
+
+    def get_final_image_size(self, img):
+        final_aspect_ratio = img.height / img.width
+        w = self.image.width
+        if self.args.output_size:
+            w = self.args.output_size
+        return int(w), int(w * final_aspect_ratio)
 
     def __repr__(self):
         return (
@@ -97,6 +121,7 @@ class FramedImage:
             f"art_h_cm={self.art_h_cm:.2f}\n"
             f"art_w_px={self.art_w_px}\n"
             f"art_h_px={self.art_h_px}\n"
+            f"pasp_border_w_px={self.pasp_border_width_px:.2f}\n"
             f"pasp_w_cm={self.pasp_w_cm:.2f}\n"
             f"pasp_h_cm={self.pasp_h_cm:.2f}\n"
             f"frame_w_cm={self.frame_w_cm:.2f}\n"
--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+import os
+
+
 def log(msg, verbose):
     if verbose:
         print(msg)
+
+
+def get_new_filename(filepath):
+    try:
+        org_filename = os.path.basename(filepath)
+        new_filename = f"framed-{org_filename}"
+        new_filepath = os.path.join(os.path.dirname(filepath), new_filename)
+        return new_filepath
+    except Exception:
+        return "framed-testimg.jpg"
